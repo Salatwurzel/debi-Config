b2cc1c5f1e103102d067d24f095071071449e4c0
Salatwurzel <salatwurzel@proton.me>
2026-03-23 02:57:51 +0100

    added initial window size

--- composeApp/src/jvmMain/kotlin/org/debi_config/main.kt
+++ composeApp/src/jvmMain/kotlin/org/debi_config/main.kt
@@ -1,7 +1,10 @@
 package org.debi_config
 
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 import org.debi_config.UI.loopUpdateVars
 import java.util.Locale
 import kotlin.system.exitProcess
@@ -15,6 +18,7 @@ fun main() = application {
     Window(
         onCloseRequest = {exitProcess(0)},
         title = "debi-Config",
+        state = rememberWindowState(size = DpSize(820.dp, 775.dp)),
     ) {
         loopUpdateVars()
         mainGUI()
