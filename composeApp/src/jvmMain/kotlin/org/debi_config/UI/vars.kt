1f9e99b2c458146b34d39e5abbbabb3e87cb42c3
Salatwurzel <salatwurzel@proton.me>
2026-03-23 05:13:26 +0100

    added check if Flathub is installed in loopUpdateVars()

--- composeApp/src/jvmMain/kotlin/org/debi_config/UI/vars.kt
+++ composeApp/src/jvmMain/kotlin/org/debi_config/UI/vars.kt
@@ -4,17 +4,20 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.unit.dp
 import java.nio.file.Files
 import java.nio.file.Paths
+import java.util.Locale.getDefault
+
+var mainPageBlur = mutableStateOf(0.dp)
 
 var btnGnomeSoftwareIsEnabled = mutableStateOf(true)
+var flathubIsInstalled = mutableStateOf(false)
 
 
 var showDialogPleaseWait = mutableStateOf(false)
 var showDialogConfirmFlatpak = mutableStateOf(false)
+var showDialogConfirmSteam = mutableStateOf(false)
 var showDialogFinishedUpdate = mutableStateOf(false)
 var showDialogFinishedFlatpak = mutableStateOf(false)
-var showDialogConfirmSteam = mutableStateOf(false)
-
-var mainPageBlur = mutableStateOf(0.dp)
+var ShowDialogFinishedSteam = mutableStateOf(false)
 
 
 
@@ -24,12 +27,20 @@ fun loopUpdateVars(){
     Thread.ofVirtual().start(){
         while(true){
 
-            when(Files.exists(Paths.get("/home/user/test.txt"))){
-                true -> {btnGnomeSoftwareIsEnabled.value = true}
-                false -> {btnGnomeSoftwareIsEnabled.value = false}
+            //FLATHUB ENABLED CHECKER
+            when(Files.exists(Paths.get("/var/lib/flatpak/repo/config"))){
+                true -> {
+                    Files.newBufferedReader(Paths.get("/var/lib/flatpak/repo/config")).forEachLine {
+                        when (it.lowercase(getDefault()).contains("flathub")){
+                            true -> {flathubIsInstalled.value = true}
+                            false -> {flathubIsInstalled.value = false}
+                        }
+                    }
+                }
+                false -> { flathubIsInstalled.value = false }
             }
 
-            Thread.sleep(1000)
+            Thread.sleep(2500)
         }
     }
 }
