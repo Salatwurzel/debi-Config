71cc5c0605e8c63cbd3029c71dc8aa19bc9f251e
Salatwurzel <salatwurzel@proton.me>
2026-03-23 02:08:26 +0100

    small changes

--- composeApp/src/jvmMain/kotlin/org/debi_config/UI/dialogConfirmFlatpak.kt
+++ composeApp/src/jvmMain/kotlin/org/debi_config/UI/dialogConfirmFlatpak.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.HorizontalDivider
