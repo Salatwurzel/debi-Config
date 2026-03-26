c6c2cbb8e2bc67906eae7ae1f3630ce534285846
Salatwurzel <salatwurzel@proton.me>
2026-03-26 13:03:10 +0100

    UI changes, added rounded border

--- composeApp/src/jvmMain/kotlin/org/debi_config/ui/DialogWorking.kt
+++ composeApp/src/jvmMain/kotlin/org/debi_config/ui/DialogWorking.kt
@@ -5,7 +5,9 @@ package org.debi_config.ui
 import androidx.compose.animation.animateColor
 import androidx.compose.animation.core.*
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.BasicAlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
@@ -15,6 +17,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.DialogProperties
@@ -36,10 +39,11 @@ class DialogWorking {
     }
 
 
+    @Preview
     @Composable
     fun init(){
 //        if (isVisible.value == true){mainPageBlur.value = 50.dp}
-        mainPageBlur.value = 50.dp
+       mainPageBlur.value = 50.dp
         BasicAlertDialog(
             onDismissRequest = {isVisible.value = false; mainPageBlur.value = 0.dp},
             Modifier.fillMaxSize(),
@@ -57,15 +61,23 @@ class DialogWorking {
                     ), label = "color"
                 )
 
-                Column(Modifier.width(300.dp).background(animatedBackgroundColor), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Text("-----------------------------------", color =Color.Green)
-                    Text("Working", fontSize = 24.sp, color = Color.Green)
-                    Text("-----------------------------------", color = Color.Green)
+                Column(Modifier.width(300.dp), horizontalAlignment = Alignment.CenterHorizontally) {
+                    Column(modifier = Modifier
+                        .fillMaxWidth()
+                        .background(animatedBackgroundColor)
+                        .border(1.dp, Color.Green, shape = RoundedCornerShape(4.dp)),
+                        horizontalAlignment = Alignment.CenterHorizontally){
+                            Text("-----------------------------------", color =Color.Green)
+                            Text("Working", fontSize = 24.sp, color = Color.Green)
+                            Text("-----------------------------------", color = Color.Green)
+                    }
                     if (statusMessage.value.isNotEmpty()) {
-                        Box(Modifier.fillMaxWidth(), contentAlignment = Alignment.CenterStart) {
+                        Spacer(Modifier.height(10.dp))
+                        Box(Modifier.fillMaxWidth().background(Color(0x1D498E39)), contentAlignment = Alignment.CenterStart) {
                             Text("Status: ${statusMessage.value}", color = Color.Green)
                         }
                     }
+                    Spacer(Modifier.height(25.dp))
                 }
             }
         }
