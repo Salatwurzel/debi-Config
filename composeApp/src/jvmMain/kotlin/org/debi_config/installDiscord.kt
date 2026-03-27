0dc4ff6457c8b83879d86434ee711fe5e45132e6
Salatwurzel <salatwurzel@proton.me>
2026-03-27 04:01:52 +0100

    added installDiscord() function

---
+++ composeApp/src/jvmMain/kotlin/org/debi_config/installDiscord.kt
@@ -0,0 +1,26 @@
+@file:Suppress("SimplifyBooleanWithConstants")
+
+package org.debi_config
+
+import org.debi_config.ui.DialogMessage
+import org.debi_config.ui.flathubIsInstalled
+import java.nio.file.Files
+import java.nio.file.Path
+import java.util.Locale
+
+val successMessage: String get() {
+    val message = "Discord installed successfully!"
+    if (Locale.getDefault().language == "de") {
+        return "Discord wurde erfolgreich installiert!"
+    } else{
+        return message
+    }
+}
+
+fun installDiscord(){
+    if (flathubIsInstalled.value == false){
+        pkExecNewTerminal("sudo apt install flatpak -y; sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo")
+    }
+    newTerminal("flatpak install -y flathub com.discordapp.Discord")
+    DialogMessage.showMessage("Discord installed successfully!")
+}
