04b9f098faa2e95ff88e94a30715192a1f91431b
Salatwurzel <salatwurzel@proton.me>
2026-03-27 05:55:05 +0100

    created and finished installDiscord()

--- composeApp/src/jvmMain/kotlin/org/debi_config/installDiscord.kt
+++ composeApp/src/jvmMain/kotlin/org/debi_config/installDiscord.kt
@@ -2,11 +2,14 @@
 
 package org.debi_config
 
+import androidx.compose.ui.unit.dp
 import org.debi_config.ui.DialogMessage
+import org.debi_config.ui.DialogWorking
 import org.debi_config.ui.flathubIsInstalled
+import org.debi_config.ui.mainPageBlur
 import java.util.*
 
-val successMessage: String get() {
+private val successMessage: String get() {
     val message = "Discord installed successfully!"
     if (Locale.getDefault().language == "de") {
         return "Discord wurde erfolgreich installiert!"
@@ -16,9 +19,18 @@ val successMessage: String get() {
 }
 
 fun installDiscord(){
+    mainPageBlur.value = 75.dp
+    DialogWorking.setStatusMessage("Downloading and installing Discord...")
+    DialogWorking.setVisible(true)
+
     if (flathubIsInstalled.value == false){
         pkExecNewTerminal("sudo apt install flatpak -y; sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo")
+    } else{
+        newTerminal("flatpak install -y flathub com.discordapp.Discord")
     }
-    newTerminal("flatpak install -y flathub com.discordapp.Discord")
+
+    DialogWorking.setStatusMessage("")
+    DialogWorking.setVisible(false)
+    mainPageBlur.value = 0.dp
     DialogMessage.showMessage(successMessage)
 }
