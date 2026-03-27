071feb0c6fd03cd3661ca7118766586059220139
Salatwurzel <salatwurzel@proton.me>
2026-03-27 05:53:42 +0100

    installSteam() finished

--- composeApp/src/jvmMain/kotlin/org/debi_config/installSteam.kt
+++ composeApp/src/jvmMain/kotlin/org/debi_config/installSteam.kt
@@ -1,18 +1,48 @@
+@file:Suppress("SimplifyBooleanWithConstants", "LiftReturnOrAssignment")
+@file:OptIn(DelicateCoroutinesApi::class)
+
 package org.debi_config
 
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import org.debi_config.ui.DialogMessage
+import org.debi_config.ui.DialogWorking
+import org.debi_config.ui.flathubIsInstalled
 import org.debi_config.ui.mainPageBlur
 import org.debi_config.ui.showDialogPleaseWait
+import java.util.Locale
+
+private val successMessage: String get(){
+    val message = "Steam installed successfully!"
+    if (Locale.getDefault().language == "de") {
+        return "Steam wurde erfolgreich installiert!"
+    } else{
+        return message
+    }
+}
 
 fun installSteam(){
-    Thread.ofVirtual().start(){
-        mainPageBlur.value = 50.dp
-        showDialogPleaseWait.value = true
+    GlobalScope.launch {
+        mainPageBlur.value = 75.dp
+        DialogWorking.setStatusMessage("Downloading and installing Steam...")
+        DialogWorking.setVisible(true)
 
-        newTerminal("pkexec /usr/bin/bash -c 'sudo apt install flatpak -y, sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo'; flatpak install com.valvesoftware.Steam -y")
+        if (flathubIsInstalled.value == false){
+            GlobalScope.launch {
+                pkExecNewTerminal("sudo apt install flatpak -y; sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo")
+                newTerminal("flatpak install -y flathub com.valvesoftware.Steam")
+            }
+        } else{
+            GlobalScope.launch {
+                newTerminal("flatpak install -y flathub com.valvesoftware.Steam")
+            }
+        }
 
         mainPageBlur.value = 0.dp
-        showDialogPleaseWait.value = false
-
+        DialogWorking.setStatusMessage("")
+        DialogWorking.setVisible(false)
+        DialogMessage.showMessage(successMessage)
     }
 }
